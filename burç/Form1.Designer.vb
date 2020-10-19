f9f2e25e494fa06d3b0311640dc2148804ca2a4b
EmreYuksell <equesse78@gmail.com>
2020-10-19 17:10:01 +0300

    Proje dosyası ekle.

---
+++ burç/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.burc = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(92, 20)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Dogum tarihiniz"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Burcunuz"
+        '
+        'burc
+        '
+        Me.burc.Location = New System.Drawing.Point(92, 74)
+        Me.burc.Name = "burc"
+        Me.burc.Size = New System.Drawing.Size(200, 20)
+        Me.burc.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 173)
+        Me.Controls.Add(Me.burc)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents burc As TextBox
+End Class
---
+++ burç/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Dim gun, ay As Integer
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        gun = DateTimePicker1.Value.Day 'Sadece gün bilgisi alınıyor.
+        ay = DateTimePicker1.Value.Month 'Sadece ay bilgisi alınıyor.
+        If (ay = 12 And gun >= 23) Or (ay = 1 And gun <= 20) Then
+            burc.Text = "OĞLAK"
+        End If/
+        If (ay = 9 And gun >= 22) Or (ay = 10 And gun <= 23) Then
+            burc.Text = "TERAZİ"
+        End If
+        If (ay = 1 And gun >= 21) Or (ay = 2 And gun <= 19) Then
+            burc.Text = "KOVA"
+        End If
+        If (ay = 2 And gun >= 20) Or (ay = 3 And gun <= 20) Then
+            burc.Text = "BALIK"
+        End If
+        If (ay = 3 And gun >= 21) Or (ay = 4 And gun <= 20) Then
+            burc.Text = "KOÇ"
+        End If
+        If (ay = 4 And gun >= 21) Or (ay = 5 And gun <= 21) Then
+            burc.Text = "BOĞA"
+        End If
+        If (ay = 5 And gun >= 22) Or (ay = 6 And gun <= 21) Then
+            burc.Text = "İKİZLER"
+        End If
+        If (ay = 6 And gun >= 22) Or (ay = 7 And gun <= 23) Then
+            burc.Text = "YENGEÇ"
+        End If
+        If (ay = 7 And gun >= 24) Or (ay = 8 And gun <= 23) Then
+            burc.Text = "ASLAN"
+        End If
+        If (ay = 8 And gun >= 24) Or (ay = 9 And gun <= 23) Then
+            burc.Text = "BAŞAK"
+        End If
+        If (ay = 10 And gun >= 23) Or (ay = 11 And gun <= 22) Then
+            burc.Text = "AKREP"
+        End If
+        If (ay = 11 And gun >= 23) Or (ay = 12 And gun <= 22) Then
+            burc.Text = "YAY"
+        End If
+    End Sub
+
+
+End Class
